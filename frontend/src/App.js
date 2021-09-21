d4107be851840e68612a8fa8e649963d31d3feb1
Lawrence Wong <lawrencelewiswong@gmail.com>
2021-09-21 15:12:53 +0800

    Tuning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -8,40 +7,6 @@ import TransactionEdit from "./TransactionEdit";
 
 class App extends Component {
 
-//	state = {
-//		transactions: []
-//	};
-//
-//	async componentDidMount() {
-//		const response = await fetch('/transactions');
-//		const body = await response.json();
-//		this.setState({ transactions: body });
-//	}
-//
-//	render() {
-//		const { transactions } = this.state;
-//		return (
-//			<div className="App">
-//				<header className="App-header">
-//					<img src={logo} className="App-logo" alt="logo" />
-//					<div className="App-intro">
-//						<h2>Transactions</h2>
-//						{transactions.map(transaction =>
-//							<div key={transaction.id}>
-//								{transaction.trackingId} ({transaction.userId},
-//	                    {transaction.currencyFrom}, {transaction.currencyTo},
-//	                    {transaction.amountSell}, {transaction.amountBuy},
-//	                    {transaction.rate}, {transaction.timePlaced},
-//	                    {transaction.originalCountry}
-//	                    )
-//	                  </div>
-//						)}
-//					</div>
-//				</header>
-//			</div>
-//		);
-//	}
-//
 	render() {
 		return (
 			<Router>
